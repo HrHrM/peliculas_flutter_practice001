34f0657d23f7a7b9869e26567b1c500f9aa0293b
Johnny <johnny130205@gmail.com>
2022-06-06 22:32:27 -0400

    Reemplazar FutureBuilder con StreamBuilder en populares

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:componentes/src/models/movie_model.dart';
 import 'package:componentes/src/providers/movie_horizontal.dart';
 import 'package:componentes/src/providers/peliculas_providers.dart';
+import 'package:componentes/src/search/search_delegate.dart';
 import 'package:componentes/src/widgets/card_swiper_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    peliculasProvider.getPopulares();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Phosphophyllite - Peliculas'),
@@ -19,17 +22,21 @@ class HomePage extends StatelessWidget {
             icon: const Icon(Icons.search),
             onPressed: () {
               print('Search button pressed');
+              showSearch(context: context, delegate: DataSearch());
             },
           ),
         ],
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _swiperTarjetas(),
-            _footer(context),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.only(top: 10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _swiperTarjetas(),
+              _footer(context),
+            ],
+          ),
         ),
       ),
     );
@@ -63,13 +70,14 @@ class HomePage extends StatelessWidget {
               child: Text('Populares',
                   style: Theme.of(context).textTheme.subtitle1)),
           const SizedBox(height: 10.0),
-          FutureBuilder(
-            future: peliculasProvider.getPopulares(),
+          StreamBuilder(
+            stream: peliculasProvider.popularesStream,
             builder:
                 (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
               if (snapshot.hasData) {
                 return MovieHorizontal(
                   peliculas: snapshot.data ?? [],
+                  siguientePagina: peliculasProvider.getPopulares,
                 );
               } else {
                 return const SizedBox(
