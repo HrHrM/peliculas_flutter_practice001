51beedd67dc83bf4345ad31325fc5d42029d77c3
Johnny <johnny130205@gmail.com>
2022-06-06 22:51:18 -0400

    Crear funciones para obtener peliculas en searchbar y Cast

--- lib/src/providers/peliculas_providers.dart
+++ lib/src/providers/peliculas_providers.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:convert';
+import 'package:componentes/src/models/actores_model.dart';
 import 'package:http/http.dart' as http;
 import 'package:componentes/src/models/movie_model.dart';
 
@@ -6,6 +8,25 @@ class PeliculasProvider {
   final String _apiKey = '751e1d5030830bb1a0cabd6127c08bcb';
   final String _url = 'api.themoviedb.org';
   final String _language = 'es-ES';
+
+  int _popularesPage = 0;
+
+  bool _cargando = false;
+
+  List<Pelicula> _populares = [];
+
+  final _popularesStreamController =
+      StreamController<List<Pelicula>>.broadcast();
+
+  Function(List<Pelicula>) get popularesSink =>
+      _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream =>
+      _popularesStreamController.stream;
+
+  void disposeStreams() {
+    _popularesStreamController.close();
+  }
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
     final resp = await http.get(url);
@@ -29,9 +50,42 @@ class PeliculasProvider {
   }
 
   Future<List<Pelicula>> getPopulares() async {
+    if (_cargando) return [];
+    _cargando = true;
+
+    _popularesPage++;
+    print('Cargando siguientes////////////////////////');
+
     final url = Uri.https(_url, '3/movie/popular', {
       'api_key': _apiKey,
       'language': _language,
+      'page': _popularesPage.toString(),
+    });
+
+    final resp = await _procesarRespuesta(url);
+
+    _populares.addAll(resp);
+    popularesSink(_populares);
+    _cargando = false;
+    return resp;
+  }
+
+  Future<List<Actor>> getCast(String peliId) async {
+    final url = Uri.https(_url, '3/movie/$peliId/credits', {
+      'api_key': _apiKey,
+      'language': _language,
+    });
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+    final cast = Actores.fromJsonList(decodedData['cast']);
+    return cast.items;
+  }
+
+  Future<List<Pelicula>> buscarPelicula(String query) async {
+    final url = Uri.https(_url, '3/search/movie', {
+      'api_key': _apiKey,
+      'language': _language,
+      'query': query,
     });
 
     return await _procesarRespuesta(url);
